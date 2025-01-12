bb0a6a532459d05bd673454470218bc5854e5b87
Rasel Shikder <46960192+raselShikderDev@users.noreply.github.com>
2025-01-12 14:45:57 +0000

    lib - 0.3

---
+++ src/lib/dCconnect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+type ConnectionObject = {
+    isConnected? : Number
+}
+
+const connection:ConnectionObject = {}
+
+ async function dbConnect(): Promise<void>{
+    try {
+       if (connection.isConnected) {
+        console.log("Database already Connected")
+        return
+       } 
+       const db = await mongoose.connect(process.env.MONGO_URI || '')
+       connection.isConnected = db.connections[0].readyState
+       console.log(connection.isConnected)
+       console.log("Database Connected Successfully")
+    } catch (error) {
+        console.error(`Database Connection faild: ${error}`)
+        process.exit(1)
+    }
+ }
+
+ default export dbConnect
